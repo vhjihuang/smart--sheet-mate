05547cf8553d0f920960750aa40c40b8f5d83625
vhjihuang <1076891184@qq.com>
2026-04-05 17:11:27 +0800

    build(config): add code splitting for vendor libraries

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,37 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rolldownOptions: {
+      output: {
+        codeSplitting: {
+          minSize: 20000,
+          groups: [
+            {
+              name: 'excel-libs',
+              test: /node_modules[\/](xlsx)/,
+              priority: 30
+            },
+            {
+              name: 'dnd-libs',
+              test: /node_modules[\/](@dnd-kit)/,
+              priority: 20
+            },
+            {
+              name: 'radix-libs',
+              test: /node_modules[\/](@radix-ui)/,
+              priority: 15
+            },
+            {
+              name: 'vendor',
+              test: /node_modules/,
+              priority: 10
+            }
+          ]
+        }
+      }
+    },
+    chunkSizeWarningLimit: 600
   },
   resolve: {
     alias: {
